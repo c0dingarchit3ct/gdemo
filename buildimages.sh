58eea94edcdfe6bc17d1bb59ff0687e2e0e1fb4d
c0dingarchit3ct <c0dingarchit3ct@gmail.com>
2017-10-23 17:23:36 -0400

    first deployment

--- buildimages.sh
+++ buildimages.sh
@@ -1,7 +1,7 @@
 
 echo "Building autocomplete image"
-docker build --force-rm -f microautocomplete/Dockerfile -t gcr.io/google_containers/autocomplete microautocomplete/
+docker build --force-rm -f microautocomplete/Dockerfile -t gcr.io/google_containers/klf-autocomplete:v1 microautocomplete/
 echo "Building sidecar image"
-docker build --force-rm -f sidecar/Dockerfile -t gcr.io/google_containers/sidecar sidecar/
+docker build --force-rm -f sidecar/Dockerfile -t gcr.io/google_containers/klf-redis-sidecar:v1 sidecar/
 echo "Building web image"
-docker build --force-rm -f web/Dockerfile -t gcr.io/google_containers/web web/
+docker build --force-rm -f web/Dockerfile -t gcr.io/google_containers/klf-web:v1 web/
